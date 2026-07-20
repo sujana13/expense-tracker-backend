12828197d37cb2d34ea3490f9fc91ddde4f78efa
Sujana Nagaraj <sujana.nagaraj@readyeducation.com~>
2026-07-20 13:34:10 +0530

    Use Render DATABASE_URL

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str
 
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-    DB_USER: str
-    DB_PASSWORD: str
+    DATABASE_URL: str
 
     SECRET_KEY: str
 
@@ -19,3 +15,26 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+#  from pydantic_settings import BaseSettings
+
+
+# class Settings(BaseSettings):
+#     APP_NAME: str
+
+#     DB_HOST: str
+#     DB_PORT: int
+#     DB_NAME: str
+#     DB_USER: str
+#     DB_PASSWORD: str
+
+#     SECRET_KEY: str
+
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+#     class Config:
+#         env_file = ".env"
+
+
+# settings = Settings()
