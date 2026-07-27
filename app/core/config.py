574f983b1f5dea306b6707e0dcd4327e0d0d42f0
Sujana Nagaraj <sujana.nagaraj@readyeducation.com~>
2026-07-27 14:00:16 +0530

    Remove Gmail SMTP configuration

--- app/core/config.py
+++ app/core/config.py
@@ -16,16 +16,6 @@ class Settings(BaseSettings):
 
     RESEND_API_KEY: str
 
-    # Email Configuration
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-
-    MAIL_SERVER: str
-    MAIL_PORT: int
-
-    MAIL_STARTTLS: bool
-    MAIL_SSL_TLS: bool
 
     class Config:
         env_file = ".env"
